a19c5369491707fba584a94e8c790fa3178aa22f
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2021-05-25 21:34:58 -0300

    add mapParser

---
+++ prolog/mapParser.py
@@ -0,0 +1,25 @@
+map = open("AKITO_-_Sakura_Kagetsu_ZZHBOY_easy.hmania", "r")
+
+texto = map.readlines()
+
+dic = {}
+novaLista = []
+
+for i in range(0,len(texto)):
+    var = texto[i].split(' ')
+    var.pop()
+    var.pop()
+    for j in range(0,len(var)):
+        if(j == 0):
+            var[j] = (int(var[j]))//40
+        var[j] = int(var[j])
+    if((var[0],var[1]) not in dic):
+        novaLista.append(var)
+        dic[(var[0],var[1])] = 1
+    
+
+mapParsed = open("AKITO_-_Sakura_Kagetsu_ZZHBOY_easy.pmania", "w")
+mapParsed.write(str(novaLista))
+
+map.close()
+mapParsed.close()
